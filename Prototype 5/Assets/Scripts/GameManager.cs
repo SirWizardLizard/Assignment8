e36cd1a7286118ab63a8c991060d45d80f3cf98b
Zechariah Burrus <zechburrus@gmail.com>
2022-04-21 12:21:28 -0500

    prototype finished

---
+++ Prototype 5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+    public List<GameObject> targets;
+
+    private float spawnRate = 1.0f;
+    private int score;
+    
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+
+    public bool isGameActive;
+
+    public Button restartButton;
+
+    public GameObject titleScreen;
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    public void StartGame(int difficulty) {
+        spawnRate /= difficulty;
+        isGameActive = true;
+        titleScreen.gameObject.SetActive(false);
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        updateScore(0);
+    }
+    public void RestartGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    
+    public void GameOver() {
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+    
+    public void updateScore(int scoreToAdd) {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+    
+    IEnumerator SpawnTarget() {
+        while (isGameActive) {
+            //Wait for the spawn rate
+            yield return new WaitForSeconds(spawnRate);
+            
+            //Choose a random target and spawn it
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+    
+    // Update is called once per frame
+    void Update() {
+    }
+}
---
+++ Prototype 5/Assets/Scripts/Target.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Resources;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Target : MonoBehaviour {
+    private Rigidbody targetRB;
+
+    private float minSpeed = 15;
+    private float maxSpeed = 20;
+
+    private float minTorque = -15;
+    private float maxTorque = 25;
+
+    private float xRange = 4;
+    private float ySpawnPos = -6;
+
+    private GameManager gameManager;
+
+    public int pointValue;
+
+    public ParticleSystem explosionParticle;
+    
+    // Start is called before the first frame update
+    void Start() {
+        targetRB = GetComponent<Rigidbody>();
+        
+        //Add an upward force with a random speed
+        targetRB.AddForce(RandomForce(), ForceMode.Impulse);
+        
+        //Add a torque force with randomized values
+        targetRB.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(),
+            ForceMode.Impulse);
+        
+        //Set the position with a random X value.
+        transform.position = RandomSpawnPos();
+        
+        //Set reference to game manager
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+    
+    #region PositionAndForces
+    private Vector3 RandomSpawnPos() {
+        //Returns a random spawn position
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+    
+    private float RandomTorque() {
+        //Returns a random Torque
+        return Random.Range(minTorque, maxTorque);
+    }
+
+    private Vector3 RandomForce() {
+        //Returns a random upwards force
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+    #endregion
+
+    #region HandleObjectDestruction
+    private void OnMouseDown() {
+        if (gameManager.isGameActive) {
+            gameManager.updateScore(pointValue);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (!gameObject.CompareTag("Bad")) {
+            gameManager.GameOver();
+        }
+        Destroy(gameObject);
+    }
+    #endregion
+}
